621c977f35e80bf25cd0c0ff668a3756532c9577
Milton Araújo <miltonjr.dev@gmail.com>
2022-07-29 13:33:02 -0300

    Improvement: Centralization of titles and descriptions - close #10

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     /*ABOUT US SECTION + container*/
     <div className="container mx-auto py-8 flex flex-col md:flex-row">
-      <div className="py-8 w-full md:w-5/12 px-3">
+      <div className="py-8 w-full md:w-5/12 px-3 text-center sm:text-start">
         <h1 className="text-slate-800 text-4xl font-semibold">
           Game Mind Studios: Quem Somos
         </h1>
