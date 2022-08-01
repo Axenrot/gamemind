f638f7086e5f8408a669acccf9e2927b059cbd01
Milton Araújo <miltonjr.dev@gmail.com>
2022-08-01 18:42:11 -0300

    Feat: Implemented blog post read page - closed #9

--- components/SectionBlog.jsx
+++ components/SectionBlog.jsx
@@ -6,7 +6,7 @@ export const SectionBlog = () => {
   return (
     <section>
       <div className="container flex flex-wrap mx-auto py-16">
-        <div className="w-full mb-8 px-4 text-center sm:px-8">
+        <div className="w-full mb-8 px-4 text-center sm:text-start sm:px-8">
           <h2 className="text-3xl sm:text-5xl font-bold tracking-wide leading-relaxed text-slate-800">
             Confira nosso blog
           </h2>
@@ -16,8 +16,8 @@ export const SectionBlog = () => {
           </p>
         </div>
 
-        <div className="w-full sm:w-1/2 px-4 sm:px-8">
-          <Link href="/">
+        <div className="w-full sm:w-1/2 px-4 sm:pl-8">
+          <Link href="/blog">
             <>
               <img
                 className="rounded-xl w-full"
@@ -25,7 +25,7 @@ export const SectionBlog = () => {
                 alt="blog-section"
               />
               <div className="rounded-xl bg-white p-6 mx-4 relative -top-8 shadow-lg">
-                <h3 className="font-bold text-lg tracking-wide mb-2 text-slate-800">
+                <h3 className="font-bold text-base tracking-wide mb-2 text-slate-800">
                   Como gravar e disponibilizar videoaulas utilizando o Canva
                 </h3>
                 <p className="text-blue-900 font-semibold">Leia Mais</p>
@@ -34,18 +34,52 @@ export const SectionBlog = () => {
           </Link>
         </div>
 
-        <div className="w-full sm:w-1/2 flex flex-wrap px-4 sm:px-8">
-          <Link className="h-1/3 border border-red-800" href=" ">
+        <div className="w-full sm:w-1/2 flex flex-wrap px-4 sm:pr-8">
+          <Link href=" ">
             <>
               <img
-                className="w-full sm:w-90 rounded-xl"
-                src="/images/blog-link.jpg"
+                className="object-cover sm:h-24 sm:w-1/2 lg:w-1/3 rounded-xl shadow-lg mb-4"
+                src="/images/img-port.png"
                 alt="image-blog-link"
               />
-              <h3 className="text-base font-bold text-slate-800 tracking-wide leading-relaxed my-5">
-                5 Jogos comerciais para auxiliar nas aulas de Português
-              </h3>
-              <p>“ O importante não é ver o que ninguém nunca viu, mas,...</p>
+              <div className="sm:w-1/2 lg:w-2/3 sm:pl-6 px-2">
+                <h3 className="text-base font-bold text-slate-800 tracking-wide leading-relaxed mb-3">
+                  5 Jogos comerciais para auxiliar nas aulas de Português
+                </h3>
+                <p>“O importante não é ver o que ninguém nunca viu, mas...</p>
+              </div>
+            </>
+          </Link>
+          <Link href=" ">
+            <>
+              <img
+                className="object-cover sm:h-24 sm:w-1/2 lg:w-1/3 rounded-xl shadow-lg mb-4 mt-4"
+                src="/images/img-mate.png"
+                alt="image-blog-link"
+              />
+              <div className="sm:w-1/2 lg:w-2/3 sm:pl-6 px-2">
+                <h3 className="text-base font-bold text-slate-800 tracking-wide leading-relaxed mb-3">
+                  5 Jogos que favorecem a compreensão da Matemática
+                </h3>
+                <p>“Desde os tempos remotos, o ser humano observa as leis...</p>
+              </div>
+            </>
+          </Link>
+          <Link href=" ">
+            <>
+              <img
+                className="object-cover sm:h-24 sm:w-1/2 lg:w-1/3 rounded-xl shadow-lg mb-4 mt-4"
+                src="/images/img-game.png"
+                alt="image-blog-link"
+              />
+              <div className="sm:w-1/2 lg:w-2/3 sm:pl-6 px-2">
+                <h3 className="text-base font-bold text-slate-800 tracking-wide leading-relaxed mb-3">
+                  Game Design para Jogos Educacionais: o que você precisa saber
+                </h3>
+                <p>
+                  “Os games fazem parte da rotina dos jovens em todo o mundo...
+                </p>
+              </div>
             </>
           </Link>
         </div>
