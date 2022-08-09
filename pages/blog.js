f3a1d25d548ffefb53347aed6c83ad7eca218807
Yuri Leon <yurileon@hotmail.com.br>
2022-08-08 23:40:52 -0300

    minor build fix

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import Footer from '../components/layout/Footer'
+import Header from '../components/layout/Header'
 
 export const Home = () => {
   return (
